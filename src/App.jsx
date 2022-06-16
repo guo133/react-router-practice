a6004d4dbf4c8e2f8928c904f6519d5f5733d01a
guo133 <104018898+guo133@users.noreply.github.com>
2022-06-17 01:11:04 +0900

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+import "./style.css";
 
 export const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+      <Page1 />
+      <Page2 />
+    </div>
+  );
 };
