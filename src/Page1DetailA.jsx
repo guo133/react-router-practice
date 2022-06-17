a672fd36237f9f2fb31635287e855961a15ea36a
guo133 <104018898+guo133@users.noreply.github.com>
2022-06-18 00:58:47 +0900

    complete!

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,17 @@
+import { useLocation, useHistory } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+
+  const history = useHistory();
+
+  const onClickBack = () => history.goBack();
   return (
     <div>
       <h1>Page1DetailAページです</h1>
+      <br />
+      <button onClick={onClickBack}>Back</button>
     </div>
   );
 };
